fbc52b21aedd589ec0a9876b2c5015f96bddd618
unknown <hishubh09@gmail.com>
2025-09-17 02:56:20 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 // start server
 const app = require("./src/app");
 const connectDb = require("./src/db/db");
+require("dotenv").config(); // ise likhna padega after npm i dotenv, warna process.env walo ki value undefined aayegi
 
 connectDb();
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("server running on port 3000");
 });
