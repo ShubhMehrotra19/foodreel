ed0ad6fc48cd8c7c72720f5aa3360f90ca7a7e9b
unknown <hishubh09@gmail.com>
2025-09-17 02:48:57 +0530

    feat: create login route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -38,6 +38,38 @@ async function registerUser(req, res) {
   });
 }
 
-async function loginUser(req, res) {}
+async function loginUser(req, res) {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      message: "User Not Found",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Invalid Email or Password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    "88f623abf0f14fdd486e52136e813a2cac27acae"
+  );
+
+  res.cookie("token", token);
+  res.status(200).json({
+    message: "User Logged in Successfully",
+    user: {
+      id: user._id,
+      email: user.email,
+      fullName: user.fullName,
+    },
+  });
+}
 
 module.exports = { registerUser, loginUser };
