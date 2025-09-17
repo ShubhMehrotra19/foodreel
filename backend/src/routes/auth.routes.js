05d54e8ecfe1c3890083984d62075128b74e89f9
unknown <hishubh09@gmail.com>
2025-09-18 04:49:51 +0530

    feat: project complete:

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
-// user auth routes
+const router = express.Router();
 
-router.post("/user/register", authController.registerUser); // yaha pe ham (req, res) => {} ko use karne ki jagah controllers wale folder mein ek controller bana rahe hai
+// user auth APIs
+router.post("/user/register", authController.registerUser);
 router.post("/user/login", authController.loginUser);
 router.get("/user/logout", authController.logoutUser);
 
-// food-partner auth routes
+// food partner auth APIs
 router.post("/food-partner/register", authController.registerFoodPartner);
 router.post("/food-partner/login", authController.loginFoodPartner);
 router.get("/food-partner/logout", authController.logoutFoodPartner);
